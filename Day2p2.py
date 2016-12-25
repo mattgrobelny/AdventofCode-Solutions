0c51d8b00058058198eee30d65b0bb0fa1f3a8ac
mattgrobelny <mattgrobelny@gmail.com>
2016-12-25 14:18:41 -0600

    day2p2 solution

---
+++ Day2p2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# Advent of Code: Day 2 part 2
+# Matt Grobelny
+
+import sys
+import numpy
+
+inputfile = "./day2_input.txt"
+fh = open(inputfile, 'r')
+
+
+w, h = 7, 7
+bucket = [[int(0)] * w for i in range(h)]
+
+bucket[1][3] = 1
+# fill number pad
+for i in range(2, 5):
+    bucket[2][i] = i
+for i in range(1, 6):
+    bucket[3][i] = i + 4
+bucket[4][2] = "A"
+bucket[4][3] = "B"
+bucket[4][4] = "C"
+bucket[5][3] = "D"
+
+print bucket
+# stat loc
+cur_x = 2
+cur_y = 2
+passcode = ""
+
+last_loc = 0
+# last_val = 0
+# print bucket
+def passout(input_text):
+    for i in range(len(input_text)):
+        global cur_x
+        global cur_y
+        global passcode
+        dir_letter = input_text[i]
+        current_val = 0
+        last_val = bucket[cur_y][cur_x]
+        cur_x_it = cur_x
+        cur_y_it = cur_y
+        if dir_letter == 'U':
+            cur_y -= 1
+        elif dir_letter == 'D':
+            cur_y += 1
+        elif dir_letter == 'L':
+            cur_x -= 1
+        elif dir_letter == 'R':
+            cur_x += 1
+
+        if bucket[cur_y][cur_x] == 0:
+            cur_x = cur_x_it
+            cur_y = cur_y_it
+            # passcode = passcode + str(bucket[cur_y][cur_x])
+            # print dir_letter, last_val, bucket[cur_y][cur_x], passcode
+
+        else:
+            continue
+            # print dir_letter, last_val, bucket[cur_y][cur_x], passcode
+    # Output last key number
+    passcode = passcode + str(bucket[cur_y][cur_x])
+    return passcode
+    return cur_x
+    return cur_y
+
+input_text1 = ""
+for line in fh:
+    passout(line)
+print "passcode"
+print passcode
